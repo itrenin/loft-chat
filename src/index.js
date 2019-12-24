a9cc902106e40330501061098319daedacf0ef9f
Ilya Trenin <itrenin@gmail.com>
2019-12-24 23:44:04 +0300

    верстка в процессе

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import 'normalize.css';
-import './style/auth.scss';
 import './style/index.scss';
+// import { addDnd } from './js/addDnd';
+
+// const chat = document.querySelector('#chat');
+
+// chat.setAttribute('data-position', [e.clientX, e.clientY])
+// addDnd(chat);
+
---
+++ src/js/addDnd.js
@@ -0,0 +1,28 @@
+export function addDnd(elem) {
+    let currentDrag;
+
+    elem.draggable = true;    
+
+    elem.addEventListener('dragstart', (e) => {
+        currentDrag = { position: [e.clientX, e.clientY] };  
+    });
+    
+    document.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+
+    document.addEventListener('drop', (e) => {
+        if (currentDrag) {
+            // let firstPosition = elem.dataset.position.split(','); 
+            let firstPosition = e.clientX + ',' + e.clientY;
+            let newPosition = [(firstPosition[0] - currentDrag.position[0] + e.clientX),
+                (firstPosition[1] - currentDrag.position[1] + e.clientY)];
+
+            elem.setAttribute('data-position', newPosition);
+
+            elem.style.left = newPosition[0] + 'px';
+            elem.style.top = newPosition[1] + 'px';
+            elem.style.transform = 'none';
+        }
+    });   
+}
